46050a332a19f87ebc905a6e74757a2b949955f9
David Åström <davidastrom@outlook.com>
2021-03-10 16:50:08 +0100

    Client: Added basic routing and dev tools

--- vue_client/src/App.vue
+++ vue_client/src/App.vue
@@ -1,27 +1,26 @@
 <template>
-	<img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <router-view />
 </template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Options({
-	components: {
-		HelloWorld
-	}
-})
-export default class App extends Vue {}
-</script>
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ vue_client/src/views/Map.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <MapView />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MapView from "@/components/MapView.vue";
+
+export default {
+  name: "Map",
+  components: {
+    MapView
+  }
+};
+</script>
