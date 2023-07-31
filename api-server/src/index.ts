b3a2cd82c8ce736393c0388f676b59697587c25e
David <davas593>
2023-07-31 23:35:29 +0200

    SSL fixes

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -16,10 +16,6 @@ async function main() {
     if (process.env.NODE_ENV != 'production') {
         dotenv.config();
     }
-    const key = fs.readFileSync(__dirname + '/../ssl/selfsigned.key');
-    const cert = fs.readFileSync(__dirname + '/../ssl/selfsigned.crt');
-
-    const credentials = { key: key, cert: cert };
 
     const app = express();
     await dbSetup(process.env.MONGODB_CONNECTION_STRING ?? '');
@@ -71,6 +67,16 @@ async function main() {
 
     app.use(errorHandler)
     app.use(notFoundHandler)
+
+    let credentials = {}
+
+    if (process.env.NODE_ENV == 'local') {
+    
+        const key = fs.readFileSync(__dirname + '/../ssl/selfsigned.key');
+        const cert = fs.readFileSync(__dirname + '/../ssl/selfsigned.crt');
+
+        credentials = { key: key, cert: cert };
+    }
 
     const httpsServer = https.createServer(credentials, app);
 
