19f1c0923c144b83124db541a27401aca22323a1
David <davas593>
2024-03-03 13:46:38 +0100

    Add button back to map from formatting

--- vue-client/src/views/harbors/ManageHarborView.vue
+++ vue-client/src/views/harbors/ManageHarborView.vue
@@ -2,9 +2,22 @@
   <div class="flex justify-center w-full">
     <Card class="w-full max-w-lg gap-2 my-8">
       <template #title>
-        <h1 class="text-2xl font-medium">
-          {{ t('harbor.manageHarbors') }}
-        </h1>
+        <div class="flex justify-between">
+          <h1 class="text-2xl font-medium">
+            {{ t('harbor.manageHarbors') }}
+          </h1>
+          <RouterLink
+            to="/"
+          >
+            <Button
+              icon="fa-solid fa-arrow-left"
+              :label="t('ui.backToMap')"
+              size="small"
+              severity="secondary"
+              >
+            </Button>
+          </RouterLink>        
+        </div>
       </template>
       <template #content>
         <div class="-mb-5">
@@ -52,6 +65,7 @@
   import { useHarborAdminStore, NEW_HARBOR_ID } from '@/stores/harborAdmin';
   import AddHarborFormVue from '@/components/forms/AddHarborForm.vue';
   import Card from 'primevue/card';
+import { RouterLink } from 'vue-router';
 
   const { t } = useI18n();
   const harborStore = useHarborStore();
